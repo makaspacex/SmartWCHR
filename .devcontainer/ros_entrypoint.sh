208adbcea47c7cc0402c56b1df0bb25055b3b5c7
jetson <jetson@yzzc.com>
2024-06-22 21:42:26 +0800

    optimized docker

--- .devcontainer/ros_entrypoint.sh
+++ .devcontainer/ros_entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 
 # setup ros2 environment
 source /opt/ros/"$ROS_DISTRO"/setup.bash --
-source ~/ros2_ws/install/setup.bash --
+source ~/ros2_ws/install/local_setup.bash --
 
 # add sourcing to .bashrc
 echo "source '/opt/ros/$ROS_DISTRO/setup.bash'" >> ~/.bashrc
-echo "source '~/ros2_ws/install/setup.bash'" >> ~/.bashrc
+echo "source '~/ros2_ws/install/local_setup.bash'" >> ~/.bashrc
 
 exec "$@"
