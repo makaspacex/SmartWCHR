23049193af0f1be10e8b562183c04102e69303af
jetson <jetson@yzzc.com>
2024-06-27 16:54:54 +0800

    增加启动功能包

---
+++ src/swcmain/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'swcmain'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
