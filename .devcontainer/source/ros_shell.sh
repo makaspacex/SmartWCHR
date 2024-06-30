dc8a5c0361d015830dbba9fc98153f1a0cbb6560
izhangxm <izhangxm@gmail.com>
2024-06-30 09:16:40 +0800

    镜像中增加自动补全，与tranfome相关的包

--- .devcontainer/source/ros_shell.sh
+++ .devcontainer/source/ros_shell.sh
@@ -10,5 +10,15 @@ function load_ros2() {
         . /opt/ros/"$ROS_DISTRO"/setup.zsh
     fi
     load_ws_ros2
+
+    # 自动补全
+    # 需要安装 sudo apt install python3-argcomplete
+    source /usr/share/colcon_cd/function/colcon_cd.sh
+    export _colcon_cd_root=/opt/ros/humble/
+    source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.zsh
+    # argcomplete for ros2 & colcon
+    eval "$(register-python-argcomplete3 ros2)"
+    eval "$(register-python-argcomplete3 colcon)"
 }
+
 load_ros2
