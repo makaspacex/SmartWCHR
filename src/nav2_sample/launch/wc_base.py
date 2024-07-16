1e39f359b74c4dc5fea54e60d02eb5b4fb2c6ebc
b4deyuqi <qyciuui@163.com>
2024-07-16 21:05:28 +0800

    代码优化

--- src/nav2_sample/launch/wc_base.py
+++ src/nav2_sample/launch/wc_base.py
@@ -2,44 +2,47 @@ import os
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
-from launch.substitutions import Command
 from launch_ros.parameter_descriptions import ParameterValue
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import Command, LaunchConfiguration
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
-    package_name = 'nav2_sample'
-    urdf_name = "wheelchair_base.urdf"
-    pkg_share = FindPackageShare(package=package_name).find(package_name)
-    urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-    
-    robot_description = Command([
-        FindExecutable(name='xacro'),
-        ' ',  # xacro和文件路径之间的空格
-        urdf_model_path
-    ])
-    parameters=[{'use_sim_time':use_sim_time, 'robot_description': robot_description, 'use_gui': False}]
-    
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=parameters,
-        )
 
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'use_gui': False, 'use_sim_time':use_sim_time,}],
-        )
-    
-    ld = LaunchDescription()
-    ld.add_action(robot_state_publisher_node)
-    ld.add_action(joint_state_publisher_node)
+    package_name = Path(__file__).parent.parent.stem
+    package_share_dir = get_package_share_directory(package_name)
 
-    return ld
+    urdf_model_path = os.path.join(package_share_dir, "urdf/wheelchair_base.urdf")
+    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
+
+    robot_description = ParameterValue(
+        Command(["xacro ", str(urdf_model_path)]), value_type=str
+    )
+
+    return LaunchDescription(
+        [
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                parameters=[
+                    {
+                        "use_sim_time": use_sim_time,
+                        "robot_description": robot_description,
+                        "use_gui": False,
+                    }
+                ],
+            ),
+            Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+                parameters=[
+                    {
+                        "use_gui": False,
+                        "use_sim_time": use_sim_time,
+                    }
+                ],
+            ),
+        ]
+    )
--- src/wcmodel/launch/wc_base.py
+++ src/wcmodel/launch/wc_base.py
@@ -2,64 +2,47 @@ import os
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    package_name = 'wcmodel'
-    urdf_name = "wheelchair_base.urdf"
 
-    ld = LaunchDescription()
-    # pkg_share = FindPackageShare(package=package_name).find(package_name) 
-    # urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
+    package_name = Path(__file__).parent.parent.stem
+    package_share_dir = get_package_share_directory(package_name)
 
-    # current_file_path = os.path.dirname(os.path.abspath(__file__))
-    # urdf_directory = os.path.join(current_file_path, '..', '..', 'urdf')
-    # urdf_directory = os.path.normpath(urdf_directory)
-    # urdf_model_path = os.path.join(urdf_directory, urdf_name)
+    urdf_model_path = os.path.join(package_share_dir, "urdf/wheelchair_base.urdf")
+    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
 
-    pkg_share = FindPackageShare(package=package_name).find(package_name)
-    urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
-    # test = 'test.urdf'
-    # urdf_model_path = os.path.join(pkg_share, f'urdf/{test}')
+    robot_description = ParameterValue(
+        Command(["xacro ", str(urdf_model_path)]), value_type=str
+    )
 
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        arguments=[urdf_model_path]
-        )
-
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        arguments=[urdf_model_path]
-        )
-
-    # static_transform_publisher_nodes = [
-    #     Node(
-    #         package='tf2_ros',
-    #         executable='static_transform_publisher',
-    #         arguments=["0", "0", "0.54", "0", "0", "0", "base_footprint", "base_link"]
-    #     ),
-    #     Node(
-    #         package='tf2_ros',
-    #         executable='static_transform_publisher',
-    #         arguments=["0.4", "0.3", "0.1", "0", "0", "0", "base_link", "laser_link"]
-    #     ),
-    #     Node(
-    #         package='tf2_ros',
-    #         executable='static_transform_publisher',
-    #         arguments=["0", "0", "0.3", "0", "0", "0", "base_link", "imu_link"]
-    #     )
-    # ]
-
-
-    ld.add_action(robot_state_publisher_node)
-    ld.add_action(joint_state_publisher_node)
-    # ld.add_action(rviz2_node)
-
-    # for static_transform_publisher_node in static_transform_publisher_nodes:
-    #     ld.add_action(static_transform_publisher_node)
-
-    return ld
+    return LaunchDescription(
+        [
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                parameters=[
+                    {
+                        "use_sim_time": use_sim_time,
+                        "robot_description": robot_description,
+                        "use_gui": False,
+                    }
+                ],
+            ),
+            Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+                parameters=[
+                    {
+                        "use_gui": False,
+                        "use_sim_time": use_sim_time,
+                    }
+                ],
+            ),
+        ]
+    )
