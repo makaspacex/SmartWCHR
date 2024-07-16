6c0f3aaa579669c8041ba64a1adc886179486858
b4deyuqi <qyciuui@163.com>
2024-07-16 21:17:40 +0800

    更新wcmodel

--- src/wcmodel/setup.py
+++ src/wcmodel/setup.py
@@ -12,6 +12,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/**')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/**')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/**')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
